a34969d583337f8d3e48f32aed8139b4bedb9647
Uditya Raj <udityaraj2002@gmail.com>
2023-02-10 22:00:08 +0530

    some Firebase imports

--- lib/widgets/imagebox.dart
+++ lib/widgets/imagebox.dart
@@ -1,7 +1,6 @@
-import 'dart:io';
 import 'dart:async';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:image_picker/image_picker.dart';
 
 class ImageBox extends StatefulWidget {
@@ -14,6 +13,9 @@ class ImageBox extends StatefulWidget {
 class _ImageBoxState extends State<ImageBox> {
   // ignore: prefer_typing_uninitialized_variables
   var _image;
+  bool savebuttonpressed = false;
+  bool _isUploading = false;
+  var _uploadedFileURL;
 
   Future getImage() async {
     var image = await ImagePicker.pickImage(source: ImageSource.gallery);
@@ -23,33 +25,63 @@ class _ImageBoxState extends State<ImageBox> {
     });
   }
 
+  Future uploadImage() async {
+    setState(() {
+      _isUploading = true;
+    });
+
+    var storageReference = FirebaseStorage.instance
+        .ref()
+        .child('images/${DateTime.now().millisecondsSinceEpoch}.jpg');
+    var uploadTask = storageReference.putFile(_image);
+    await uploadTask.whenComplete;
+    storageReference.getDownloadURL().then((fileURL) {
+      setState(() {
+        _uploadedFileURL = fileURL;
+        _isUploading = false;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          _image == null
-              ? const Text('No image selected.')
-              : Container(
-                  height: MediaQuery.of(context).size.height / 1.6,
-                  width: MediaQuery.of(context).size.width / 1.4,
-                  decoration: BoxDecoration(
-                    image: DecorationImage(
-                      image: FileImage(_image),
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-          const SizedBox(
-            height: 20.0,
-          ),
-          ElevatedButton(
-            onPressed: getImage,
-            child: const Text('Image Box'),
-          ),
-        ],
-      ),
+    return Column(
+      children: [
+        InkWell(
+          onTap: getImage,
+          child: _isUploading
+              ? const CircularProgressIndicator()
+              : _uploadedFileURL != null
+                  ? Image.network(
+                      _uploadedFileURL,
+                      height: 200,
+                      width: 200,
+                    )
+                  : _image == null
+                      ? Container(
+                          color: const Color.fromARGB(255, 132, 217, 134),
+                          height: MediaQuery.of(context).size.height / 2.5,
+                          width: MediaQuery.of(context).size.width / 1.4,
+                          child: const Center(
+                            child: Text(
+                              'Tap here to select Image!!',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 20),
+                            ),
+                          ),
+                        )
+                      : Container(
+                          height: MediaQuery.of(context).size.height / 2.5,
+                          width: MediaQuery.of(context).size.width / 1.4,
+                          decoration: BoxDecoration(
+                            image: DecorationImage(
+                              image: FileImage(_image),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+        ),
+      ],
     );
   }
 }
